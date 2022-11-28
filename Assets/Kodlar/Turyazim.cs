4cbae9b997147c1d584ca6370c6e83e45fa88932
Ömer Faruk KONCA <190541018@firat.edu.tr>
2022-11-28 20:50:29 +0300

    update

---
+++ Assets/Kodlar/Turyazim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Turyazim : MonoBehaviour
+{
+    public int tur;
+    [SerializeField] TextMeshProUGUI tursayac;
+    [SerializeField] TextMeshProUGUI Kazanma;
+
+    void Start()
+    {
+        tur = PlayerPrefs.GetInt("tur");
+        tursayac.text = tur.ToString();
+        if(tur <= 30)
+        { Kazanma.text = ("Yasasin aklindan tuttugun sayiyi buldum.."); }
+        PlayerPrefs.SetInt("tur", 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
